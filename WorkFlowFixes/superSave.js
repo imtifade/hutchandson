4ecfeae60fda98ddb3c69daa84480ce9a1ef8245
Kyle <Kyle@HUTCHNET.local>
2020-02-21 08:44:22 -0600

    superSave Refactored

--- WorkFlowFixes/superSave.js
+++ WorkFlowFixes/superSave.js
@@ -11,191 +11,192 @@ It checks the super save status then generates the correct records with record.t
 It throws a new line of html in the webpage to get the redirects done. 
 */
 
-define(['N/record', "N/ui/serverWidget"],
-    function (record, serverWidget) {
-        function beforeLoad(context) {
-            //if the record is being created (not saved yet) do this
-            if (context.type == 'create') {
-                //grab super save status
-                var superSaved = context.newRecord.getValue({
-                    fieldId: 'custbody_supersaved'
-                });
-                //if super save is checked then do this
-                if (superSaved) {
-                    //grab the internal id of the sales order the fulfillment
-                    var createdfrom = context.newRecord.getValue({
-                        fieldId: 'createdfrom'
-                    });
-                    //load the original sales order to an object
-                    var originalSO = record.load({
-                        type: record.Type.SALES_ORDER,
-                        id: createdfrom,
-                        isDynamic: true,
-                    });
-                    //load the terms of payment
-                    terms = originalSO.getValue({
-                        fieldId: 'terms'
-                    });
+define(['N/record', "N/ui/serverWidget"], runsave);
+function runsave(record, serverWidget) {
+    recordModule = record;
+    serverWidgetModule = serverWidget;
 
-                    //if the terms are unset or set to due on reciept 
-                    if (!terms || terms == 4) {
-                        
-                        cashSaleGen (createdfrom, originalSO, context);
-                    
-                    }
-                    
-                    //if it's not suppose to be a cash sale do this
-                    else {
-                        
-                        invoiceGen (createdfrom, context);
-
-                    }
-
-                }
-
-            }
-        }
-
-        return {
-            beforeLoad: beforeLoad
-        }
-
-        function cashSaleGen (createdfrom, originalSO, context){
-            //check if the payment type is TBD
-            var paymentType = originalSO.getValue({
-                fieldId: 'custbody3'
+    var returnObj = {};
+    returnObj.beforeLoad = superSave;
+    return returnObj;
+}
+function superSave(context) {
+    //if the record is being created (not saved yet) do this
+    if (context.type == 'create') {
+        //grab super save status
+        var superSaved = context.newRecord.getValue({
+            fieldId: 'custbody_supersaved'
+        });
+        //if super save is checked then do this
+        if (superSaved) {
+            //grab the internal id of the sales order the fulfillment
+            var createdfrom = context.newRecord.getValue({
+                fieldId: 'createdfrom'
             });
-                //turn it in to an item fulfillment
-                var objFulfillment = record.transform({
-                    fromType: record.Type.SALES_ORDER,
-                    fromId: createdfrom,
-                    toType: record.Type.ITEM_FULFILLMENT,
-                    isDynamic: true,
-                });
-                //set ship status to shipped
-                objFulfillment.setValue({
-                    fieldId: 'shipstatus',
-                    value: "C",
-                    ignoreFieldChange: true,
-                    forceSyncSourcing: true
-                });
-                //save the fulfillment
-                var fulfillmentID = objFulfillment.save({
-                    enableSourcing: true,
-                    ignoreMandatoryFields: true
-                });
-                //turn the same sales order in to a cash sale.
-                var objCashSale = record.transform({
-                    fromType: record.Type.SALES_ORDER,
-                    fromId: createdfrom,
-                    toType: record.Type.CASH_SALE,
-                    isDynamic: true,
-                });
-                //save the cash sale
-                var cashSaleID = objCashSale.save({
-                    enableSourcing: true,
-                    ignoreMandatoryFields: true
-                });
+            //load the original sales order to an object
+            var originalSO = recordModule.load({
+                type: recordModule.Type.SALES_ORDER,
+                id: createdfrom,
+                isDynamic: true,
+            });
+            //load the terms of payment
+            terms = originalSO.getValue({
+                fieldId: 'terms'
+            });
 
-                //if it is TBD only redirect
-            if (paymentType == "TBD") {
-                
-                var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/cashsale.nl?id=' + cashSaleID + '&whence=&e=T';
+            //if the terms are unset or set to due on reciept 
+            if (!terms || terms == 4) {
 
-                var redirect2_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
+                cashSaleGen(createdfrom, originalSO, context);
 
-                var field2 = context.form.addField({
-                    id: 'custpage_redirect2',
-                    type: serverWidget.FieldType.INLINEHTML,
-                    label: 'Redirect2'
-                });
-
-                field2.defaultValue = redirect2_value;
-                
             }
-            //else redirect to cash sale and print form
+
+            //if it's not suppose to be a cash sale do this
             else {
 
-                //https://1204410.app.netsuite.com/app/accounting/print/hotprint.nl?regular=T&sethotprinter=T&formnumber=136&trantype=cashsale&&id=49986&label=Cash+Sale&printtype=transaction
-
-                var printURL = 'https://1204410.app.netsuite.com/app/accounting/print/hotprint.nl?regular=T&sethotprinter=T&formnumber=136&trantype=cashsale&&id=' + cashSaleID + "&label=Cash+Sale&printtype=transaction";
-
-                var redirect_value = '<html><body><script language="javascript">window.open("' + printURL + '", "mywindow","location=1,status=1,scrollbars=1, resizable=1, directories=1, toolbar=1, titlebar=1");</script></body></html>';
-
-                var field = context.form.addField({
-                    id: 'custpage_redirect',
-                    type: serverWidget.FieldType.INLINEHTML,
-                    label: 'Redirect'
-                });
-
-
-                field.defaultValue = redirect_value;
-
-                var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/cashsale.nl?id=' + cashSaleID + '&whence=';
-
-                var redirect2_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
-
-                var field2 = context.form.addField({
-                    id: 'custpage_redirect2',
-                    type: serverWidget.FieldType.INLINEHTML,
-                    label: 'Redirect2'
-                });
-
-
-                field2.defaultValue = redirect2_value;
+                invoiceGen(createdfrom, context);
 
             }
-        }
-
-        function invoiceGen (createdfrom, context) {
-
-            //turn it in to an item fulfillment
-            var objFulfillment = record.transform({
-                fromType: record.Type.SALES_ORDER,
-                fromId: createdfrom,
-                toType: record.Type.ITEM_FULFILLMENT,
-                isDynamic: true,
-            });
-            //set the ship status to shipped
-            objFulfillment.setValue({
-                fieldId: 'shipstatus',
-                value: "C",
-                ignoreFieldChange: true,
-                forceSyncSourcing: true
-            });
-            //save the fulfillment
-            var fulfillmentID = objFulfillment.save({
-                enableSourcing: true,
-                ignoreMandatoryFields: true
-            });
-            //turn the sales order in to an invoice
-            var objInvoice = record.transform({
-                fromType: record.Type.SALES_ORDER,
-                fromId: createdfrom,
-                toType: record.Type.INVOICE,
-                isDynamic: true,
-            });
-            //save the invoice
-            var InvoiceID = objInvoice.save({
-                enableSourcing: true,
-                ignoreMandatoryFields: true
-            }); 
-
-            //redirect using inserted HTML to the new invoice
-
-            var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/custinvc.nl?id=' + InvoiceID + '&e=T&whence=';
-
-            var redirect_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
-
-            var field = context.form.addField({
-                id: 'custpage_redirect',
-                type: serverWidget.FieldType.INLINEHTML,
-                label: 'Redirect'
-            });
-
-
-            field.defaultValue = redirect_value;
 
         }
 
+    }
+}
+
+function cashSaleGen(createdfrom, originalSO, context) {
+    //check if the payment type is TBD
+    var paymentType = originalSO.getValue({
+        fieldId: 'custbody3'
     });
+    //turn it in to an item fulfillment
+    var objFulfillment = recordModule.transform({
+        fromType: recordModule.Type.SALES_ORDER,
+        fromId: createdfrom,
+        toType: recordModule.Type.ITEM_FULFILLMENT,
+        isDynamic: true,
+    });
+    //set ship status to shipped
+    objFulfillment.setValue({
+        fieldId: 'shipstatus',
+        value: "C",
+        ignoreFieldChange: true,
+        forceSyncSourcing: true
+    });
+    //save the fulfillment
+    var fulfillmentID = objFulfillment.save({
+        enableSourcing: true,
+        ignoreMandatoryFields: true
+    });
+    //turn the same sales order in to a cash sale.
+    var objCashSale = recordModule.transform({
+        fromType: recordModule.Type.SALES_ORDER,
+        fromId: createdfrom,
+        toType: recordModule.Type.CASH_SALE,
+        isDynamic: true,
+    });
+    //save the cash sale
+    var cashSaleID = objCashSale.save({
+        enableSourcing: true,
+        ignoreMandatoryFields: true
+    });
+
+    //if it is TBD only redirect
+    if (paymentType == "TBD") {
+
+        var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/cashsale.nl?id=' + cashSaleID + '&whence=&e=T';
+
+        var redirect2_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
+
+        var field2 = context.form.addField({
+            id: 'custpage_redirect2',
+            type: serverWidgetModule.FieldType.INLINEHTML,
+            label: 'Redirect2'
+        });
+
+        field2.defaultValue = redirect2_value;
+
+    }
+    //else redirect to cash sale and print form
+    else {
+
+        //https://1204410.app.netsuite.com/app/accounting/print/hotprint.nl?regular=T&sethotprinter=T&formnumber=136&trantype=cashsale&&id=49986&label=Cash+Sale&printtype=transaction
+
+        var printURL = 'https://1204410.app.netsuite.com/app/accounting/print/hotprint.nl?regular=T&sethotprinter=T&formnumber=136&trantype=cashsale&&id=' + cashSaleID + "&label=Cash+Sale&printtype=transaction";
+
+        var redirect_value = '<html><body><script language="javascript">window.open("' + printURL + '", "mywindow","location=1,status=1,scrollbars=1, resizable=1, directories=1, toolbar=1, titlebar=1");</script></body></html>';
+
+        var field = context.form.addField({
+            id: 'custpage_redirect',
+            type: serverWidgetModule.FieldType.INLINEHTML,
+            label: 'Redirect'
+        });
+
+
+        field.defaultValue = redirect_value;
+
+        var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/cashsale.nl?id=' + cashSaleID + '&whence=';
+
+        var redirect2_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
+
+        var field2 = context.form.addField({
+            id: 'custpage_redirect2',
+            type: serverWidgetModule.FieldType.INLINEHTML,
+            label: 'Redirect2'
+        });
+
+
+        field2.defaultValue = redirect2_value;
+
+    }
+}
+
+function invoiceGen(createdfrom, context) {
+
+    //turn it in to an item fulfillment
+    var objFulfillment = recordModule.transform({
+        fromType: recordModule.Type.SALES_ORDER,
+        fromId: createdfrom,
+        toType: recordModule.Type.ITEM_FULFILLMENT,
+        isDynamic: true,
+    });
+    //set the ship status to shipped
+    objFulfillment.setValue({
+        fieldId: 'shipstatus',
+        value: "C",
+        ignoreFieldChange: true,
+        forceSyncSourcing: true
+    });
+    //save the fulfillment
+    var fulfillmentID = objFulfillment.save({
+        enableSourcing: true,
+        ignoreMandatoryFields: true
+    });
+    //turn the sales order in to an invoice
+    var objInvoice = recordModule.transform({
+        fromType: recordModule.Type.SALES_ORDER,
+        fromId: createdfrom,
+        toType: recordModule.Type.INVOICE,
+        isDynamic: true,
+    });
+    //save the invoice
+    var InvoiceID = objInvoice.save({
+        enableSourcing: true,
+        ignoreMandatoryFields: true
+    });
+
+    //redirect using inserted HTML to the new invoice
+
+    var cashSaleURL = 'https://1204410.app.netsuite.com/app/accounting/transactions/custinvc.nl?id=' + InvoiceID + '&e=T&whence=';
+
+    var redirect_value = '<html><body><script language="javascript">window.location.replace("' + cashSaleURL + '");</script></body></html>';
+
+    var field = context.form.addField({
+        id: 'custpage_redirect',
+        type: serverWidgetModule.FieldType.INLINEHTML,
+        label: 'Redirect'
+    });
+
+
+    field.defaultValue = redirect_value;
+
+}
